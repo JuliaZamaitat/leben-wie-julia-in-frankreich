3ba2f354166f92e7894d637df358be516f900cd7
JuliaZamaitat <j.zamaitat@gmail.com>
2023-06-12 11:22:08 +0200

    fix rendering issue

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,50 +1,52 @@
-import fs from 'fs';
-import Markdown from 'markdown-to-jsx';
-import matter from 'gray-matter';
-// import getPostMetadata from './getPostMetadata';
+// import fs from 'fs';
+// import Markdown from 'markdown-to-jsx';
+// import matter from 'gray-matter';
+// // import getPostMetadata from './getPostMetadata';
 
-const getPostContent = (slug: string) => {
-  const folder = 'posts/';
-  const file = `${folder}${slug}.md`;
-  const content = fs.readFileSync(file, 'utf8');
-  const matterResult = matter(content);
-  return matterResult;
-};
-
-// export const generateStaticParams = async () => {
-//   const posts = getPostMetadata();
-//   return posts.map((post) => ({
-//     slug: post.slug,
-//   }));
+// const getPostContent = (slug: string) => {
+//   const folder = 'posts/';
+//   const file = `${folder}${slug}.md`;
+//   const content = fs.readFileSync(file, 'utf8');
+//   const matterResult = matter(content);
+//   return matterResult;
 // };
 
-const Post = (props: any) => {
-  const slug = props.post.slug;
-  const post = getPostContent(slug);
-  return (
-    <div>
-      <div className="my-12 text-center">
-        <h1 className="text-4xl text-rose-500">{post.data.title}</h1>
-        <h2 className="text-xl text-gray-400 pt-1">{post.data.subtitle}</h2>
-        <p className="text-rose-400">{post.data.date}</p>
-      </div>
+// // export const generateStaticParams = async () => {
+// //   const posts = getPostMetadata();
+// //   return posts.map((post) => ({
+// //     slug: post.slug,
+// //   }));
+// // };
 
-      <article className="prose mx-auto my-0 py-10">
-        <style>
-          {`
-      .prose img {
-        max-width: 100%;
-        max-height: 90vh;
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-      }
-    `}
-        </style>
-        <Markdown>{post.content}</Markdown>
-      </article>
-    </div>
-  );
-};
+// const Post = (props: any) => {
+//   const slug = props.post.slug;
+//   const post = getPostContent(slug);
+//   return (
+//     <div>
+//       <div className="my-12 text-center">
+//         <h1 className="text-4xl text-rose-500">{post.data.title}</h1>
+//         <h2 className="text-xl text-gray-400 pt-1">{post.data.subtitle}</h2>
+//         <p className="text-rose-400">{post.data.date}</p>
+//       </div>
 
-export default Post;
+//       <article className="prose mx-auto my-0 py-10">
+//         <style>
+//           {`
+//       .prose img {
+//         max-width: 100%;
+//         max-height: 90vh;
+//         display: block;
+//         margin-left: auto;
+//         margin-right: auto;
+//       }
+//     `}
+//         </style>
+//         <Markdown>{post.content}</Markdown>
+//       </article>
+//     </div>
+//   );
+// };
+
+// export default Post;
+
+export default {};
--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,12 +1,13 @@
-import { PostMetadata } from './PostMetadata';
-import Post from './Post';
+// import { PostMetadata } from './PostMetadata';
+// import Post from './Post';
 
-const PostList = (props: PostMetadata) => {
-  return (
-    <div className="p-4 bg-white">
-      <Post post={props}></Post>
-    </div>
-  );
-};
+// const PostList = (props: PostMetadata) => {
+//   return (
+//     <div className="p-4 bg-white">
+//       <Post post={props}></Post>
+//     </div>
+//   );
+// };
 
-export default PostList;
+// export default PostList;
+export default {};
