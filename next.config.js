2255c6187ccc888ee15cb94f6fa158d8e32e5349
JuliaZamaitat <j.zamaitat@gmail.com>
2023-06-12 12:03:21 +0200

    test

--- next.config.js
+++ next.config.js
@@ -5,6 +5,23 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/posts/02-06-23": {
+        page: "/posts/[slug]",
+        query: { slug: "02-06-23" },
+      },
+      "/posts/06-06-23": {
+        page: "/posts/[slug]",
+        query: { slug: "06-06-23" },
+      },
+      ...defaultPathMap, // Behalte die vordefinierten Pfade bei
+    };
+  },
 };
 
 module.exports = nextConfig;
